044f8c470ecf098bde43e92d297b27b4e86feebe
aliamanatali <aliamanatali218@gmail.com>
2024-07-03 23:12:51 +0500

    Use Effect Hook Implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import UseStatePractice from './components/UseStatePractice';
+import UseEffectPractice from './components/UseEffectPractice';
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<UseStatePractice />} />
+      <Route path="/" element={<UseStatePractice />} />
+      <Route path="/UseEffect" element={<UseEffectPractice />} />
       </Routes>
     </div>
   );
